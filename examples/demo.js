2b943710e676fff8a46c1e3758d1955989fb5cc8
allensunjian <Allen_sun_js@hotmail.com>
2022-02-17 16:27:19 +0800

    迁入

---
+++ examples/demo.js
@@ -0,0 +1,25 @@
+const { GetProcessorID, GetCpuInfo, Ping, GetIp, NetSh } = require("node-system");
+// GetProcessorID().then(cupid => {
+//     console.log(cupid)
+// })
+
+// GetCpuInfo().then(cupinfo => {
+//     console.log(cupinfo);
+// })
+// const pingClient = Ping("www.baidu.com");
+// pingClient().then(val => {
+//     console.log(val)
+// }).catch(err => {
+//     console.log(err)
+// })
+// GetIp().then(val => {
+//     console.log(123,val)
+// })
+
+const net_shell = NetSh();
+net_shell.scan().then(list => {
+    console.log(list)
+})
+net_shell.currentConnect().then(val => {
+    console.log(val)
+})
