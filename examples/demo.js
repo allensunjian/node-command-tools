4fefd1128418f4f335587eeb896f203ab3c09ecc
allensunjian <Allen_sun_js@hotmail.com>
2022-02-26 14:01:43 +0800

    commit

--- examples/demo.js
+++ examples/demo.js
@@ -13,12 +13,12 @@ const { GetProcessorID, GetCpuInfo, Ping, GetIp, NetSh } = require("../lib/node-
 //     console.log(err)
 // })
 // GetIp().then(val => {
-//     console.log(123,val)
-// })
+//     console.log(123,val) 
+// })   
 
 const net_shell = NetSh();
 net_shell.scan().then(list => {
-    console.log(list)
+    console.log(JSON.stringify(list))
 })
 // net_shell.currentConnect().then(val => {
 //     console.log(val)
@@ -29,6 +29,6 @@ net_shell.scan().then(list => {
 //         console.log("断开失败")
 //     } else {
 //         console.log(val)
-//     }
+//     }clear
 // })
 // net_shell.connnect({ssid: '痘博士_5G', password: 'douboshi'})
